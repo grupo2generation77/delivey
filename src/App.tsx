15f14a4495e429f6a8a9b54e0fc2ec37f6a8d368
Matheus <matheuspq11@gmail.com>
2025-01-31 21:38:48 -0300

    bug bizarro de animate-delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/login/Login";
+import Catalogo from "./pages/catalogo/Catalogo";
 
 function App() {
   return (
     <>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Catalogo />} />
     </Routes>
     </BrowserRouter>
     </>
