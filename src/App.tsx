0638bb1d4f4d90c9a535626bd32ecb7b99097d93
Lucas Oliveira <lucasvmo42@gmail.com>
2025-02-04 09:24:32 -0300

    sobre e navbar atualizadas

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,22 @@ import Home from "./pages/home/Home";
 import CatalogoPage from "./pages/catalogo/CatalogoPage";
 import { teste } from "./services/Service";
 import Navbar from "./components/navbar/Navbar";
+import Sobre from "./pages/sobre/Sobre";
 
 function App() {
   console.log(teste());
   return (
     <>
-        <BrowserRouter>
-          <Navbar />
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/Home" element={<Home />} />
-              <Route path="/catalogo" element={<CatalogoPage />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/catalogo" element={<CatalogoPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sobre" element={<Sobre />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
